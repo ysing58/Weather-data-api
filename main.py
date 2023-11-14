fd77469b05a384a91cda886b91a7b48f74659e9f
Yadvendra Singh <yadvendra187@gmail.com>
2023-11-15 02:04:21 +0530

    Added endpoint for yearly and station data fetching

--- main.py
+++ main.py
@@ -10,6 +10,21 @@ def home():
 
     return render_template("home.html", data = station.to_html())
 
+@app.route("/api/v1/<station>")
+def all_data(station):
+    filename = "Files/TG_STAID"+str(station).zfill(6)+".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    result = df.to_dict(orient="records")
+    return(result)
+
+@app.route("/api/v1/<station>/<year>")
+def only_year(station, year):
+    filename = "Files/TG_STAID"+str(station).zfill(6)+".txt"
+    df = pd.read_csv(filename, skiprows=20)
+    df['    DATE'] = df['    DATE'].astype(str)
+    result = df.loc[df['    DATE'].str.startswith(str(year))]
+    result = result.to_dict(orient="records")
+    return(result)
 @app.route("/api/v1/<station>/<date>")
 def about(station, date):
     filename = "Files/TG_STAID"+str(station).zfill(6)+".txt"
